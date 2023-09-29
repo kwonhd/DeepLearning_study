aca36307009a8e80b1263e174a370c04475c7b62
kwonhd <gurehd321@naver.com>
2023-09-29 17:26:22 +0900

    img_gray

--- DeepLearning/OpenCV/opencv.py
+++ DeepLearning/OpenCV/opencv.py
@@ -55,3 +55,97 @@ print(pic_copy[:,:,2].shape)
 plt.imshow(pic_copy[:,:,2],cmap='gray')
 plt.show() #블루 채널에 대한 빛정보 그레이 채널
 # %%
+# R 채널 분포
+pic_red = pic_arr.copy()
+pic_red[:,:,1] = 0
+pic_red[:,:,2] = 0
+# %%
+pic_red
+# %%
+plt.imshow(pic_red)
+plt.show()
+# %%
+pic_green = pic_arr.copy()
+pic_green[:,:,0] = 0
+pic_green[:,:,2] = 0
+#%%
+pic_green
+# %%
+plt.imshow(pic_green)
+plt.show()
+# %%
+pic_blue = pic_arr.copy()
+pic_blue[:,:,0] = 0
+pic_blue[:,:,1] = 0
+#%%
+pic_blue
+# %%
+plt.imshow(pic_blue)
+plt.show()
+# %%
+# 이미지 출력 (opencv)
+# 이미지를 창에 표시합니다
+
+cv2.imshow('', pic_arr)
+# 키 입력을 대기하고 그 다음 창을 닫습니다
+cv2.waitKey(0)
+cv2.destroyAllWindows()# %%
+#opencv 채널순서는 BGR 순서이다!!
+
+# %%
+image = cv2.cvtColor(pic_arr, cv2.COLOR_RGB2BGR) # RGB로 변경된 색
+cv2.imshow('',image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# %%
+print(image[0][0]) # BGR
+# %%
+print(pic_arr[0][0]) # RGB
+# %%
+url = 'https://cdn.pixabay.com/photo/2018/10/01/09/21/pets-3715733_960_720.jpg'
+response = requests.get(url)
+pic = Image.open(BytesIO(response.content))
+pic.save('dog.jpg') # 현재 수행중인 가상환경에 사진 저장
+# %%
+cv_image = cv2.imread('dog.jpg', cv2.IMREAD_UNCHANGED)
+# %%
+print(type(cv_image))
+# %%
+print(cv_image)
+# %%
+cv2.imshow('', cv_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# %%
+plt.imshow(cv_image)
+plt.show()
+# %%
+image_temp = cv2.cvtColor(cv_image,cv2.COLOR_BGR2RGB)
+# %%
+plt.imshow(image_temp)
+plt.show()
+# %%
+url = 'https://cdn.pixabay.com/photo/2017/01/12/21/42/tiger-1975790_1280.jpg'
+response = requests.get(url)
+pic = Image.open(BytesIO(response.content))
+pic.save('tiger.jpg')
+# %%
+img_gray = cv2.imread('tiger.jpg',cv2.IMREAD_GRAYSCALE)
+# %%
+print(img_gray.shape) # 2차배열 그레이채널
+# %%
+cv2.imshow('', img_gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# %%
+plt.imshow(img_gray)
+plt.show()
+# %%
+plt.imshow(img_gray, cmap='gray')
+plt.show()
+# %%
+plt.imshow(img_gray, cmap='magma')
+plt.show()
+
+# %%
+### 이미지 쓰기
--- DeepLearning/OpenCV/readme.md
+++ DeepLearning/OpenCV/readme.md
@@ -24,3 +24,13 @@
     - Open CV : 실시간 이미지 프로세싱에 중점
         - 이미지 읽기/쓰기(Numpy)
         - 이미지 읽기(PIL) : 코랩, 주피터는 PIL, matplotlib 적합
+
+            - 이미지 출력 : cv2.imshow
+
+            - 이미지 읽기 
+                - cv2.IMREAD_COLOR: 이미지 파일을 Color로 읽어들이고, 투명한 부분은 무시되며, Default 값
+                - cv2.IMREAD_GRAYSCALE: 이미지를 Grayscale로 읽음. 실제 이미지 처리시 중간단계로 많이 사용
+                - cv2.IMREAD_UNCHANGED: 이미지 파일을 alpha channel (투명도)까지 포함하여 읽어 들임
+                - (주의) cv2.imread()는 잘못된 경로로 읽어도 NoneType으로 들어갈 뿐, 오류를 발생하지 않음
+            - 이미지 쓰기
+                - cv2.imwrite()
